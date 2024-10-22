e409075aa7ad8ecfffc9ff44637d982960c26076
Abhilash Mahanta <abhilashmahanta824@gmail.com>
2024-10-22 20:00:21 +0530

    Create strlen().c

---
+++ Assignment (27-09-24)/strlen().c
@@ -0,0 +1,13 @@
+/*String Length*/
+
+#include<stdio.h>
+main()
+{
+char fname[25];
+int x;
+printf("Enter the string:\n");
+scanf("%s",fname);
+x=strlen(fname);
+printf("String length is %d",x);
+
+}
